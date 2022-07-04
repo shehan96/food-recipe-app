3fd72270cf0fa95f303c51d460cb66891c0b26d3
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-07-04 21:14:24 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, GestureResponderEvent, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleOnPress = (event : GestureResponderEvent) => {
+    setCount(currentCount => count + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <Text style={{margin: 8, borderWidth: 1, borderColor: 'red', padding: 16, borderRadius: 5}}>Inline Style</Text>
+      <Text style={styles.textInput}>Hello World</Text>
+      <Button title='Click Me' onPress={handleOnPress} />
+      <TouchableOpacity style={{margin: 4, padding: 4, backgroundColor: '#f9f9f9'}}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    color: '#464646',
+    fontSize: 20
+  }
 });
